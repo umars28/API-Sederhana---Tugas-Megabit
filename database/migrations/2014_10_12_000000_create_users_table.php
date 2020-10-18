ec40807f4b7e792811a7ef1f8c47de66a3fd9a6b
Umar Sabirin <umarsabirin369@gmail.com>
2020-10-18 13:27:12 +0800

    Add Schema Database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('username')->unique();
+            $table->string('fullname');
+            $table->date('birth_of_date')->nullable();
+            $table->string('birth_of_place')->nullable();
+            $table->enum('gender', ['L', 'P'])->nullable();
+            $table->enum('role', ['Admin', 'Regular']);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_10_18_051812_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->unsignedBigInteger('users_id')->nullable();
+            $table->timestamps();
+        });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreign('users_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
